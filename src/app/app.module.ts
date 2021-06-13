1c6609e915dce009adda5380cf0db5c89ff3ee23
Alvaro Bellmunt <alvaro4323@gmail.com>
2021-06-13 12:12:58 +0200

    Inicializamos los componentes, modulos y servicios necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraBusquedaComponent } from './components/barra-busqueda/barra-busqueda.component';
+import { CrearLibroComponent } from './components/crear-libro/crear-libro.component';
+import { DetallesLibroComponent } from './components/detalles-libro/detalles-libro.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListadoLibrosComponent } from './components/listado-libros/listado-libros.component';
+import { ModificarLibroComponent } from './components/modificar-libro/modificar-libro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraBusquedaComponent,
+    CrearLibroComponent,
+    DetallesLibroComponent,
+    FooterComponent,
+    ListadoLibrosComponent,
+    ModificarLibroComponent
   ],
   imports: [
     BrowserModule,
