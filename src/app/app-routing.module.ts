8f002a3294c8c1a8a8edb6a277f510fc1498e2d7
Alvaro Bellmunt <alvaro4323@gmail.com>
2021-06-13 13:04:22 +0200

    Creamos el routing para los components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearLibroComponent } from './components/crear-libro/crear-libro.component';
+import { DetallesLibroComponent } from './components/detalles-libro/detalles-libro.component';
+import { ListadoLibrosComponent } from './components/listado-libros/listado-libros.component';
+import { ModificarLibroComponent } from './components/modificar-libro/modificar-libro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListadoLibrosComponent},
+  {path: 'libro/:id', component: DetallesLibroComponent},
+  {path: 'crearlibro', component: CrearLibroComponent},
+  {path: 'modificarlibro/:id', component: ModificarLibroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
